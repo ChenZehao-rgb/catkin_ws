0241c016118b10b1548e6ea833d0801b17809be3
ChenZehao-rgb <1784359802@qq.com>
2024-07-08 17:00:15 +0800

    ros入门

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,8 +131,10 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/chen/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/chen/catkin_ws/build/learning_launch/cmake_install.cmake")
   include("/home/chen/catkin_ws/build/learning_parameter/cmake_install.cmake")
   include("/home/chen/catkin_ws/build/learning_service/cmake_install.cmake")
+  include("/home/chen/catkin_ws/build/learning_tf/cmake_install.cmake")
   include("/home/chen/catkin_ws/build/learning_topic/cmake_install.cmake")
 
 endif()
